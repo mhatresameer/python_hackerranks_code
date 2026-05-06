486d7a30b5cb24dafff586e763d4e9fe6c42a0ce
M. Sameer <mhatresameer@outlook.com>
2026-05-06 21:47:58 +0530

    Create python_operators.py

---
+++ python_operators.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    sum_number = a+b
+    difference_number = a-b
+    product_number = a*b
+    
+    print(sum_number)
+    print(difference_number)
+    print(product_number)
